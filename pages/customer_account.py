52d52565a0c0af9715996798fce1ea59110286e2
Vadim <vadimarmo@gmail.com>
2025-05-21 18:45:51 +0300

    UI_Project

--- pages/customer_account.py
+++ pages/customer_account.py
@@ -34,6 +34,3 @@ class CustomerAccount(BasePage):
             'There is already an account with this email address. '
             'If you are sure that it is your email address, '
             'click here to get your password and access your account.'), f'твое сообщение: \n{message}'
-
-    # def data_verification(self):
-    #     assert data
